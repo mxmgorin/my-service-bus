0b8f928909a46b61df7b5ea44a2a045ae623e97a
Andrey <amigin@gmail.com>
2021-10-13 03:08:17 +0300

    Removed Logs

--- src/operations/delivery/delivery.rs
+++ src/operations/delivery/delivery.rs
@@ -94,12 +94,6 @@ async fn deliver_to_queue_spawned(
         }
 
         if send_packet {
-            if topic.topic_id == "bidask" {
-                println!("Sendinf bidask queue: {}", queue.queue_id);
-                println!("Sending bidask {:?}", tcp_contract);
-                println!("--------------")
-            }
-
             crate::operations::sessions::send_package(
                 app.as_ref(),
                 subscriber_data.session.as_ref(),
